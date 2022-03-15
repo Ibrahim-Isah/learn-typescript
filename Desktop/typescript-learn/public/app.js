bdcf9851ba1e051cea1de469464c5fd492b45ec3
Isah Ibrahim <isahibn08@gmail.com>
2022-03-15 01:19:20 +0100

    third version

--- Desktop/typescript-learn/public/app.js
+++ Desktop/typescript-learn/public/app.js
@@ -1,18 +1,13 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
-let docOne;
-let docTwo;
-let docs = [];
-docOne = new Invoice('Isah', 'doings things', 400);
-docTwo = new Payment('Maimuna', 'doings things website', 200);
-docs.push(docOne);
-docs.push(docTwo);
-console.log('this the docs', docs);
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -22,5 +17,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- Desktop/typescript-learn/src/app.ts
+++ Desktop/typescript-learn/src/app.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/hasFormatter.js";
 
@@ -9,19 +10,8 @@ const toFrom = document.querySelector('#tofrom') as HTMLInputElement
 const details = document.querySelector('#details') as HTMLInputElement
 const amount = document.querySelector('#amount') as HTMLInputElement
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-let docs: HasFormatter[] = [];
-
-docOne = new Invoice('Isah' , 'doings things' , 400);
-docTwo = new Payment('Maimuna' , 'doings things website' , 200);
-
-
-docs.push(docOne)
-docs.push(docTwo)
-
-console.log('this the docs' , docs)
+const ul = document.querySelector('ul')!;
+const list =new ListTemplate(ul);
 
 form.addEventListener('submit' , (e: Event) => {
     e.preventDefault();
@@ -33,7 +23,6 @@ form.addEventListener('submit' , (e: Event) => {
     } else {
          doc =new Payment(toFrom.value, details.value, amount.valueAsNumber)
     }
-    console.log(
-        doc
-    )
+    
+    list.render(doc,type.value, 'end' )
 })
